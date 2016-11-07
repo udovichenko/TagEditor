9f87bf126406be1f49bc9bf0f54242ddda70cdd9
udovichenko <d.udovichenko@gmail.com>
2016-11-08 00:02:56 +0300

    dev setup, minor improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,65 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    // cssmin = require('gulp-cssnano'),
-    rigger = require('gulp-rigger');
+    cssmin = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    prefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
+    rigger = require('gulp-rigger'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+gulp.task('server', function() {
+    browserSync({
+        server: {
+            //     // baseDir: "./www"
+            //     // directory: true
+            //     // port: 3000
+        }
+    });
+
+    // gulp.watch([
+    //     '**/*.js',
+    //     '**/*.html'
+    // ]).on('change', function() {
+    //     browserSync.reload();
+    // });
+});
+
+gulp.task('css', function () {
+    // console.log('gulp.task(css');
+    gulp.src(['src/scss/**/*.scss'])
+        .pipe(concat('tageditor.css'))
+        //.pipe(sourcemaps.init())
+        //.pipe(rigger()) //Прогоним через rigger
+        .pipe(sass({
+            //sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({stream: true}))
+        //.pipe(cssimport({}))
+        // .pipe(importcss({}))
+        .pipe(rename("tageditor.min.css"))
+        .pipe(cssmin())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function(){
+    watch(['**/*.html'], function(event, cb) {
+        browserSync.reload();
+    });
+    watch(['src/scss/**/*.scss'], function(event, cb) {
+        gulp.start('css');
+    });
+    watch(['**/*.js'], function(event, cb) {
+        browserSync.reload();
+    });
+});
 
 gulp.task('libsConcat', function () {
     gulp.src(['src/libs.js']) //Найдем наш main файл
@@ -10,4 +68,4 @@ gulp.task('libsConcat', function () {
         .pipe(gulp.dest('demo/js')); //Выплюнем готовый файл в build
 });
 
-gulp.task('default', ['libsConcat']);
+gulp.task('default', ['libsConcat', 'css', 'server', 'watch']);
--- tageditor.js
+++ tageditor.js
@@ -1,145 +1,205 @@
-var TagEditor = function(params) {
-    if (!(this instanceof TagEditor)) return new TagEditor(params);
+(function() {
 
-    var This = this;
+    var TagEditor = function(options) {
+        //if (!(this instanceof TagEditor)) return new TagEditor(T.params);
 
-    this.defaults = {
-        separator: ','
-        //el: '.accordion'
-    };
+        var T = this;
 
-    params = $.extend({}, this.defaults, params);
+        T.defaults = {
+            separator: ',',
+            buttonClass: 'te-button'
+            //el: '.accordion'
+        };
 
-    This.separator = params.separator;
-
-    this.unique = function(list) {
-        return list.filter(function(value, index, self) {
-            return self.indexOf(value) === index;
-        });
-    };
-
-    this.getTagsFromText = function(text) {
-        // var text = This.textArea.val();
-        var textTags = text.split(This.separator);
-        var textTagsClean = [];
-
-        $.each(textTags, function(i) {
-            //console.log(i);
-            var tagTrimmed = $.trim(textTags[i]);
-            if (tagTrimmed != '') {
-                textTagsClean.push(tagTrimmed);
+        T.controls = {
+            format: {
+                text: 'Отформатировать',
+                action: 'format'
+            },
+            spaceToComma: {
+                text: '_ > ,',
+                action: 'spaceToComma'
             }
-        });
+        };
 
-        return textTagsClean;
-    };
+        T.options = $.extend({}, T.defaults, options);
 
-    this.textUnique = function() {
-        var text = This.textArea.val();
-        var textTags = this.getTagsFromText(text);
-        textTags = This.unique(textTags);
-        This.textArea.val(textTags.join(This.separator + ' '));
-        console.log(This.separator);
-    };
+        T.tags = [];
+        T.separator = T.options.separator;
 
-    this.textCheck = function() {
-        var text = This.textArea.val();
-        var textTagsClean = this.getTagsFromText(text);
+        T.init = function() {
+            T.ioArea = $(T.options.ioArea);
+            T.labelArea = $(T.options.labelArea);
+            T.controlPanel = $(T.options.controlPanel);
+            T.buttonCheck = $('.tags-check');
 
-        text = textTagsClean.join(This.separator + ' ');
-        This.textArea.val(text);
-    };
+            T.buildControls();
 
-    this.textFormat = function(text) {
+            T.restore();
+            T.textAreaResize();
+            T.count();
+            T.tags = T.getTagsFromText(T.ioText());
 
-        if (This.separator == ',') {
-            text = text.replace(/(\;|\,|\n){1,}/g, This.separator);
-        }
+            T.attachEvents();
+        };
 
-        // removes spaces before separator
-        text = text.replace(/ (\,|\;){1,}/g, ",");
-        // text = text.replace(/ {1,}/g, ",");
+        T.buildControls = function() {
+            for (var i in T.controls) {
+                var thisControl = T.controls[i];
+                if (!thisControl.action) return;
 
-        // removes double spaces
-        text = text.replace(/ {1,}/g, " ");
+                var trigger = thisControl.trigger || 'click';
+                var controlTpl = $('<button>');
+                controlTpl
+                    .addClass(T.options.buttonClass)
+                    .text(thisControl.text);
 
-        return text;
-    };
+                if (typeof T[thisControl.action] == "function") {
+                    controlTpl.on(trigger, T[thisControl.action]);
+                }
 
-    this.textAreaResize = function() {
-        // var textarea = This.textArea.get(0);
-        // var paddingV = parseInt(This.textArea.css('padding-top')) + parseInt(This.textArea.css('padding-bottom'));
-        // // console.log(paddingV);
-        // // console.log(textarea.scrollHeight);
+                T.controlPanel.append(controlTpl);
+            }
+        };
+
+        T.unique = function(list) {
+            return list.filter(function(value, index, self) {
+                return self.indexOf(value) === index;
+            });
+        };
+
+        T.getTagsFromText = function(text) {
+            // var text = This.textArea.val();
+            var textTags = text.split(T.separator);
+            var textTagsClean = [];
+
+            $.each(textTags, function(i) {
+                //console.log(i);
+                var tagTrimmed = $.trim(textTags[i]);
+                if (tagTrimmed != '') {
+                    textTagsClean.push(tagTrimmed);
+                }
+            });
+
+            return textTagsClean;
+        };
+
+        T.updateTags = function() {
+            T.tags = T.getTagsFromText(T.ioText());
+        };
+
+        T.textUnique = function() {
+            var text = T.ioText();
+            var textTags = T.getTagsFromText(text);
+            textTags = T.unique(textTags);
+            T.ioArea.val(textTags.join(T.separator + ' '));
+            console.log(T.separator);
+        };
+
+        T.textCheck = function() {
+            var text = T.ioArea.val();
+            var textTagsClean = T.getTagsFromText(text);
+
+            text = textTagsClean.join(T.separator + ' ');
+            T.ioArea.val(text);
+        };
+
+        T.ioText = function() {
+            return T.ioArea.val();
+        };
+
+        T.textFormat = function(text) {
+            if (T.separator == ',') {
+                text = text.replace(/(\;|\,|\n){1,}/g, T.separator);
+            }
+
+            // removes spaces before separator
+            text = text.replace(/ (\,|\;){1,}/g, ",");
+            // text = text.replace(/ {1,}/g, ",");
+
+            // removes double spaces
+            text = text.replace(/ {1,}/g, " ");
+
+            return text;
+        };
+        
+        T.spaceToComma = function() {
+            //console.log(T.ioArea.val().replace(/(\,)/g, ' '));
+
+            T.separator = ' ';
+            T.updateTags();
+            T.setSeparator(',');
+        };
+
+        T.setSeparator = function(separator) {
+            //console.log('spaceToComma');
+            T.separator = separator;
+
+            var newSeparator = (separator == ' ')
+                ? separator
+                : separator + ' ';
+
+            var outputText = T.tags.join(newSeparator);
+            T.ioArea.val(outputText);
+        };
+
+        T.textAreaResize = function() {
+            autosize(T.ioArea);
+        };
+
+        T.textChange = function() {
+            var text = T.ioArea.val();
+            var formatedText = T.textFormat(text);
+            T.ioArea.val(formatedText);
+        };
+
+        T.count = function() {
+            var text = T.ioArea.val();
+            var textTags = T.getTagsFromText(text);
+            $('.tags-count').text(textTags.length);
+        };
+
+        T.saveTagsFromIo = function() {
+            T.tags = T.getTagsFromText(T.ioArea.val());
+        };
+
+        T.save = function() {
+            Cookies.set('text', T.ioArea.val());
+        };
+
+        T.restore = function() {
+            var text = Cookies.get('text');
+            T.ioArea.val(text);
+        };
+
+        T.ioKeyup = function() {
+            T.textChange();
+            T.textAreaResize();
+            T.count();
+            T.save();
+        };
+
+        // T.build = function() {
         //
-        // var resultHeight = -paddingV - 4 + textarea.scrollHeight + 'px';
-        // console.log(resultHeight);
-        // This.textArea.scrollTop(0);
-        // textarea.style.overflow = 'hidden';
-        // textarea.style.height = resultHeight;
-        // // console.log(textarea.scrollHeight);
-        // // textarea.style.overflow = 'hidden';
-        // // textarea.style.height = 0;
-        // // textarea.style.height = this.scrollHeight + 'px';
-        autosize(This.textArea);
+        // };
+
+        T.attachEvents = function() {
+            $(T.ioArea).on('keyup', T.ioKeyup);
+
+            // $(T.buttonCheck).on('click', function() {
+            //     T.textCheck();
+            //     T.textUnique();
+            //     T.count();
+            //     T.save();
+            // });
+
+            $(window).on('load', function() {
+
+            });
+        };
+
+        T.init();
     };
 
-    this.textChange = function() {
-        var text = This.textArea.val();
-        var formatedText = This.textFormat(text);
-        This.textArea.val(formatedText);
-    };
-
-    this.count = function() {
-        var text = This.textArea.val();
-        var textTags = this.getTagsFromText(text);
-        $('.tags-count').text(textTags.length);
-    };
-
-    this.save = function() {
-        Cookies.set('text', This.textArea.val());
-    };
-
-    this.restore = function() {
-        var text = Cookies.get('text');
-        This.textArea.val(text);
-    };
-
-    this.textArea = $(params.textArea);
-    this.labelArea = $(params.labelArea);
-    this.buttonCheck = $('.tags-check');
-
-    $(this.textArea).on('keyup', function() {
-        This.textChange();
-        This.textAreaResize();
-        This.count();
-        This.save();
-    });
-
-    $(this.buttonCheck).on('click', function() {
-        This.textCheck();
-        This.textUnique();
-        This.count();
-        This.save();
-    });
-
-    $(window).on('load', function() {
-        This.restore();
-        This.textAreaResize();
-        This.count();
-    });
-
-
-    //
-    // $('.tags-button-separator').on('click', function() {
-    //     var self = $(this);
-    //     This.separator = self.text();
-    //     self
-    //         .addClass('active')
-    //         .siblings()
-    //         .removeClass('active');
-    //
-    //     This.textCheck();
-    // });
-
-};
+    window.TagEditor = TagEditor;
+})();
